f7586a3c9ed010f98d40792859e317383d3058f4
Zenitsu-god <pranavlande1@gmail.com>
2024-10-16 16:35:24 +0530

    head  font

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home({featuredProduct,newProducts}) {
   return (
     <div style={{paddingBottom:"20px"}}>
       <Head>
-      @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600;700&display=swap');      
+      @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600;700&display=swap");      
       </Head>
       <Header />
       <Featured product={featuredProduct} />
